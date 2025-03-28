0b7c9800b8a28d6143c34c2827d74a248ef815f2
Levykin10 <ilyalevikin11259@gmail.com>
2025-03-28 15:35:25 +0300

    ChagePasswordMainWindow.xaml.cs

--- Levykin.i.m/ChagePasswordMainWindow.xaml.cs
+++ Levykin.i.m/ChagePasswordMainWindow.xaml.cs
@@ -15,62 +15,15 @@ using System.Windows.Shapes;
 
 namespace Levykin.i.m
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class AdminWindow : Window
     {
         public AdminWindow()
         {
             InitializeComponent();
-            LoadUsers();
+           
         }
 
-        private void LoadUsers()
-        {
-            // Здесь должна быть логика загрузки пользователей из вашего источника данных
-            var users = new List<User>
-            {
-                new User { Lastname = "Иванов", firstname = "Иван", role = "Администратор", username = "ivanov", IsLocked = false },
-                new User { Lastname = "Петрова", firstname = "Мария", role = "Пользователь", username = "petrova", IsLocked = true }
-            };
-
-            Users.ItemsSource = users;
-        }
-
-        private void AddUser_Click(object sender, RoutedEventArgs e)
-        {
-            // Логика добавления нового пользователя
-            MessageBox.Show("Функция добавления пользователя");
-        }
-
-        private void DeleteUser_Click(object sender, RoutedEventArgs e)
-        {
-            if (Users.SelectedItem != null)
-            {
-                // Логика удаления выбранного пользователя
-                MessageBox.Show("Пользователь будет удален");
-            }
-            else
-            {
-                MessageBox.Show("Выберите пользователя для удаления", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-        }
-
-        private void SaveChanges_Click(object sender, RoutedEventArgs e)
-        {
-            // Логика сохранения изменений
-            MessageBox.Show("Изменения сохранены");
-        }
-    }
-
-    public class User
-    {
-        public string Lastname { get; set; }
-        public string firstname { get; set; }
-        public string role { get; set; }
-        public string username { get; set; }
-        public bool IsLocked { get; set; }
-    }  
+      
     }
 }
